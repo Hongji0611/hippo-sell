31b20c5c6c14bea9cfd74215c40548779e8d3135
Jiwoo Hong <hjw6112@gmail.com>
2022-10-23 18:45:28 +0900

    feat: Chat set-header-body-footer

--- src/containers/chatting/ChattingScreen.js
+++ src/containers/chatting/ChattingScreen.js
@@ -1,10 +1,28 @@
 import { useEffect, useState, useNavigate } from "react";
 
+import "./Chatting.css";
+
 export default function ChattingScreen() {
+    const [chatText, setChatText] = useState("");
 
     return (
         <div className="chatBox">
-            채팅
+            <div className="header">
+                <p className="headerTitle">판매하마</p>
+            </div>
+            <div className="body">
+                csdv
+            </div>
+            <div className="footer">
+                <input
+                    className="chatTextBox"
+                    type="text"
+                    name="chatText"
+                    placeholder="판매하마와 대화해보세요"
+                    onChange={(e) => setChatText(e.target.value)}
+                    value={chatText}
+                />
+            </div>
         </div>
     );
 }
