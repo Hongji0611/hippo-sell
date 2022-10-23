80d80c6fa8e95163343e056328e2fb72a9cebe88
Jiwoo Hong <hjw6112@gmail.com>
2022-10-23 17:44:20 +0900

    feat: login add-condition-at-btn

--- src/containers/login/LoginScreen.js
+++ src/containers/login/LoginScreen.js
@@ -1,12 +1,31 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
-import { Link } from "react-router-dom";
 
 export default function LoginScreen() {
 
     const [id, setId] = useState("");
     const [pw, setPw] = useState("");
     const [store, setStore] = useState("");
+
+    const navigate = useNavigate();
+
+    const userData = [
+        {
+            id: "4220011",
+            pw: "q123123123",
+            store: "주안"
+        }
+    ]
+
+    const navigateToChat = () => {
+        if (userData[0].id === id && userData[0].pw === pw && userData[0].store === store){
+            navigate("/chat");
+        }else{
+            // 모달 추가
+        }
+
+    };
 
     return (
         <div className="loginBox">
@@ -24,29 +43,29 @@ export default function LoginScreen() {
             </div>
 
             <div>
-            <input
-                className="pw"
-                type="password"
-                name="pw"
-                placeholder="비밀번호를 입력하세요"
-                onChange={(e) => setPw(e.target.value)}
-                value={pw}
-            />
+                <input
+                    className="pw"
+                    type="password"
+                    name="pw"
+                    placeholder="비밀번호를 입력하세요"
+                    onChange={(e) => setPw(e.target.value)}
+                    value={pw}
+                />
             </div>
-           
-           <div>
-           <input
-                className="store"
-                type="text"
-                name="store"
-                placeholder="지점명을 입력하세요"
-                onChange={(e) => setStore(e.target.value)}
-                value={store}
-            />
-           </div>
 
-            <button className="loginBtn">로그인</button>
-           
+            <div>
+                <input
+                    className="store"
+                    type="text"
+                    name="store"
+                    placeholder="지점명을 입력하세요"
+                    onChange={(e) => setStore(e.target.value)}
+                    value={store}
+                />
+            </div>
+
+            <button className="loginBtn" onClick={navigateToChat}>로그인</button>
+
         </div>
     );
 }
